04653bf76e0759b1beb8bec008530f4ad8e1b1e1
whatot <whatot2@gmail.com>
2013-03-05 22:54:17 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#此vimrc需要ctags,cscope等,并暂时只支持linux,其余系统未测试
+
+if [ -f "~/.vimrc"  ]; then
+  cp ~/.vimrc .vimrc-bakup
+fi
+
+mkdir -p ~/.vim/bundle/
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/whatot/whatot-vimrc.git
+
+cp whatot-vimrc/.vimrc ~/.vimrc
+vim +BundleInstall +qa
+
+#这个mru.vim是lilydjwg的修改版本,但还是有些许问题
+cp whatot-vimrc/mru.vim ~/.vim/bundle/mru.vim/plugin/mru.vim
+cp whatot-vimrc/filenametags ~/.vim/
+ctags -R -f ~/.vim/systags /usr/include /usr/local/include
