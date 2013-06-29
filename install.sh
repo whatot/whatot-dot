b6b2d0f982952ad495e6e55288e30f11af21d5e6
whatot <whatot2@gmail.com>
2013-06-29 22:16:44 +0800

    add -x and remove some useless lines

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 #
 # 此vimrc需要ctags,cscope,ycm等,并暂时只支持linux,其余系统未测试
 # git clone https://github.com/whatot/whatot-vimrc.git
@@ -11,11 +11,9 @@ mkdir -p ~/.vim/bundle/
 mkdir -p ~/.vim/sessions/
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
-git submodule update --init --recursive
-
 # 也可以使用 ln -s <source> <destination>
-cp whatot-vimrc/.vimrc ~/.vimrc
+cp .vimrc ~/.vimrc
 vim +BundleInstall +qa
 
-cp whatot-vimrc/filenametags ~/.vim/
+cp filenametags ~/.vim/
 ctags -R -f ~/.vim/systags /usr/include /usr/local/include
