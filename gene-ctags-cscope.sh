c6f72d3d07f4c5411a9d66444b40537b640335f8
whatot <whatot2@gmail.com>
2013-07-30 13:08:05 +0800

    add echofunc

--- gene-ctags-cscope.sh
+++ gene-ctags-cscope.sh
@@ -2,5 +2,5 @@
 set -x
 find . -name "*.h" -o -name "*.c" -o -name "*.cc" > cscope.files
 cscope -bkq -i cscope.files
-# ctags -R --fields=+lS
-ctags -R --c++-kinds=+px --fields=+iaS --extra=+q
+ctags -R --fields=+lS
+# ctags -R --c++-kinds=+px --fields=+iaS --extra=+q
