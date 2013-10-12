3771a34814e878b0b751d36b970b88833777be2c
whatot <whatot2@gmail.com>
2013-10-12 13:26:12 +0800

    using script to configure tmux-startup

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -1,64 +1,54 @@
 #! /bin/bash
-export AP_7x27_PROJECT="~/Developer/MSM7x27A-ICS-AP"
-export MP_7x27_PROJECT="~/Developer/MSM7x27A-ICS-MP"
+# set -x
 
-export AP_8x25_PROJECT="~/Developer/MSM8x25-ICS-AP"
-export MP_8x25_PROJECT="~/Developer/MSM8x25-ICS-MP"
+# -d           -- do not attach new session to current terminal
+# -n           -- name the initial window
+# -s           -- name the session
+# -t           -- specify target session
 
-if [ -z "$TMUX" ]; then
-    tmux has-session -t development7x27
-    if [ $? != 0 ]; then
-        # init 7x27 AP
-        tmux new-session -s development7x27 -n AP_7x27 -d
-        tmux send-keys -t development7x27 "cd $AP_7x27_PROJECT&&. ./build/envsetup.sh&&choosecombo 1 13 1" C-m
-        tmux split-window -h -p 40 -t development7x27:1
-        tmux send-keys -t development7x27 "cd $AP_7x27_PROJECT&&. ./build/envsetup.sh&&choosecombo 1 13 1" C-m
-        tmux split-window -v -t development7x27:1.2
-        tmux send-keys -t development7x27 "cd $AP_7x27_PROJECT&&. ./build/envsetup.sh&&choosecombo 1 13 1" C-m
+# /usr/bin/xfce4-terminal --maximize --execute /usr/bin/tmux.sh
+# /usr/bin/xfce4-terminal --command=tmux --maximize
 
-        # init 7x27 MP
-        tmux new-window -n MP_7x27 -t development7x27
 
-        tmux send-keys -t development7x27:2 "cd $MP_7x27_PROJECT/modem_proc/build/ms" C-m
-        tmux split-window -h -p 40 -t development7x27:2
-        tmux send-keys -t development7x27:2 "cd $MP_7x27_PROJECT/modem_proc/build/ms" C-m
-        tmux split-window -v -t development7x27:2.2
-        tmux send-keys -t development7x27 "cd $MP_7x27_PROJECT/modem_proc/build/ms" C-m
+if [ -f /usr/bin/tmux ]; then
 
-        tmux select-window -t development7x27:1
-        tmux select-pane -t development7x27:1 -L
-    fi
+	if [[ $(hostname -s) == 'mirage' ]]; then
 
-        tmux send-keys -t development7x27:1.3 "export DISPLAY=$DISPLAY" C-m
-        tmux send-keys -t development7x27:2.3 "export DISPLAY=$DISPLAY" C-m
+		tmux has-session -t works125
+		if [ $? != 0 ]; then
+			tmux new-session -s works125 -n ~git -d
+			tmux send-keys -t works125 "cd ~/git/" C-m
 
-    tmux has-session -t development8x25
-    if [ $? != 0 ]; then
-        # init 8x25 AP
-        tmux new-session -s development8x25 -n AP_8x25 -d
-        tmux send-keys -t development8x25 "cd $AP_8x25_PROJECT&&. ./build/envsetup.sh&&choosecombo 1 17 3" C-m
-        tmux split-window -h -p 40 -t development8x25:1
-        tmux send-keys -t development8x25 "cd $AP_8x25_PROJECT&&. ./build/envsetup.sh&&choosecombo 1 17 3" C-m
-        tmux send-keys -t development8x25 'top' C-m
-        tmux split-window -v -t development8x25:1.2
-        tmux send-keys -t development8x25 "cd $AP_8x25_PROJECT&&. ./build/envsetup.sh&&choosecombo 1 17 3" C-m
+			tmux new-window -n trunk -t works125
+			tmux send-keys -t works125:2 "j trunk" C-m
 
-        # init 8x25 MP
-        tmux new-window -n MP_8x25 -t development8x25
+			tmux new-window -n mac-3 -t works125
+			tmux send-keys -t works125:3 "j 3" C-m
 
-        tmux send-keys -t development8x25:2 "cd $MP_8x25_PROJECT/modem_proc/build/ms" C-m
-        tmux split-window -h -p 40 -t development8x25:2
-        tmux send-keys -t development8x25:2 "cd $MP_8x25_PROJECT/modem_proc/build/ms" C-m
-        tmux split-window -v -t development8x25:2.2
-        tmux send-keys -t development8x25 "cd $MP_8x25_PROJECT/modem_proc/build/ms" C-m
+			tmux select-window -t works125:1
+			tmux select-pane -t works125:1
+		fi
+		tmux attach -t works125
 
-        tmux select-window -t development8x25:1
-        tmux select-pane -t development8x25:1 -L
-    fi
+	elif [[ $(hostname -s) == 'cru' ]]; then
 
-        tmux send-keys -t development8x25:1.3 "export DISPLAY=$DISPLAY" C-m
-        tmux send-keys -t development8x25:2.3 "export DISPLAY=$DISPLAY" C-m
+		tmux has-session -t mydev
+		if [ $? != 0 ]; then
+			tmux new-session -s mydev -n ~git -d
+			tmux send-keys -t mydev "cd ~/git/" C-m
 
-    tmux attach -t development7x27
+			tmux new-window -n mac-3 -t mydev
+			tmux send-keys -t mydev:2 "j 3" C-m
+
+			tmux new-window -n jemalloc -t mydev
+			tmux send-keys -t mydev:3 "cd ~/git/jemalloc/include/jemalloc/internal" C-m
+
+			tmux select-window -t mydev:1
+			tmux select-pane -t mydev:1
+		fi
+		tmux attach -t mydev
+
+	fi
+
 fi
 
