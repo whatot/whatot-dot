d59db75693626b2519387e6bf3551d1ffe70e420
whatot <whatot2@gmail.com>
2013-11-01 00:30:42 +0800

    refactory the tmux/tmux.sh to functions

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -9,66 +9,91 @@
 # /usr/bin/xfce4-terminal --maximize --execute /usr/bin/tmux.sh
 # /usr/bin/xfce4-terminal --command=tmux --maximize
 
+function tmux_company {
+
+tmux has-session -t works125
+if [ $? != 0 ]; then
+	tmux new-session -s works125 -n ~git -d
+	tmux send-keys -t works125 "cd ~/git/" C-m
+
+	tmux new-window -n trunk -t works125
+	tmux send-keys -t works125:2 "j trunk" C-m
+
+	tmux new-window -n share -t works125
+	tmux send-keys -t works125:3 "cd ~/" C-m
+
+	tmux new-window -n dot -t works125
+	tmux send-keys -t works125:4 "j dot" C-m
+
+	tmux new-window -n mac-3 -t works125
+	tmux send-keys -t works125:5 "j 3" C-m
+
+	tmux new-window -n changing -t works125
+	tmux send-keys -t works125:6 "cd ~/git/changing" C-m
+
+	tmux select-window -t works125:1
+	tmux select-pane -t works125:1
+fi
+tmux attach -t works125
+
+}
+
+
+function tmux_mydev {
+
+tmux has-session -t mydev
+if [ $? != 0 ]; then
+	tmux new-session -s mydev -n '~git' -d
+	tmux send-keys -t mydev "cd ~/git/" C-m
+
+	tmux new-window -n mac-3 -t mydev
+	tmux send-keys -t mydev:2 "j 3" C-m
+
+	tmux new-window -n lua2 -t mydev
+	tmux split-window -h -t mydev
+	tmux send-keys -t mydev:3.1 "cd ~/git/redis/deps/lua/src/" C-m
+	tmux send-keys -t mydev:3.2 "cd ~/base/ma_c/5/deps/lua/src/" C-m
+
+	tmux new-window -n jemalloc -t mydev
+	tmux send-keys -t mydev:4 "cd ~/git/jemalloc/include/jemalloc/internal" C-m
+
+	tmux new-window -n changing -t mydev
+	tmux send-keys -t mydev:5 "cd ~/../mirage/static/git/changing" C-m
+
+	tmux new-window -n redis -t mydev
+	tmux send-keys -t mydev:6 "cd ~/git/redis/src/" C-m
+
+	tmux new-window -n kernel -t mydev
+	tmux send-keys -t mydev:7 "cd ~/linux/" C-m
+
+	tmux new-window -n alp -t mydev
+	tmux send-keys -t mydev:8 "cd ~/" C-m
+
+	tmux new-window -n share -t mydev
+	tmux send-keys -t mydev:9 "cd ~/" C-m
+
+	tmux select-window -t mydev:1
+	tmux select-pane -t mydev:1
+fi
+tmux attach -t mydev
+
+}
+
 
 if [ -f /usr/bin/tmux ]; then
 
-	if [[ $(hostname -s) == 'mirage' ]]; then
+	if [ $# == 0 ]; then
 
-		tmux has-session -t works125
-		if [ $? != 0 ]; then
-			tmux new-session -s works125 -n ~git -d
-			tmux send-keys -t works125 "cd ~/git/" C-m
-
-			tmux new-window -n trunk -t works125
-			tmux send-keys -t works125:2 "j trunk" C-m
-
-			tmux new-window -n mac-3 -t works125
-			tmux send-keys -t works125:3 "j 3" C-m
-
-			tmux new-window -n changing -t mydev
-			tmux send-keys -t works125:4 "cd ~/git/changing" C-m
-
-			tmux select-window -t works125:1
-			tmux select-pane -t works125:1
+		if [[ $(hostname -s) == 'mirage' ]]; then
+			tmux_company
+		elif [[ $(hostname -s) == 'cru' ]]; then
+			tmux_mydev
 		fi
-		tmux attach -t works125
 
-	elif [[ $(hostname -s) == 'cru' ]]; then
-
-		tmux has-session -t mydev
-		if [ $? != 0 ]; then
-			tmux new-session -s mydev -n '~git' -d
-			tmux send-keys -t mydev "cd ~/git/" C-m
-
-			tmux new-window -n mac-3 -t mydev
-			tmux send-keys -t mydev:2 "j 3" C-m
-
-			tmux new-window -n lua2 -t mydev
-			tmux split-window -h -t mydev
-			tmux send-keys -t mydev:3.1 "cd ~/git/redis/deps/lua/src/" C-m
-			tmux send-keys -t mydev:3.2 "cd ~/base/ma_c/5/deps/lua/src/" C-m
-
-			tmux new-window -n jemalloc -t mydev
-			tmux send-keys -t mydev:4 "cd ~/git/jemalloc/include/jemalloc/internal" C-m
-
-			tmux new-window -n changing -t mydev
-			tmux send-keys -t mydev:5 "cd ~/../mirage/static/git/changing" C-m
-
-			tmux new-window -n redis -t mydev
-			tmux send-keys -t mydev:6 "cd ~/git/redis/src/" C-m
-			
-			tmux new-window -n kernel -t mydev
-			tmux send-keys -t mydev:7 "cd ~/linux/" C-m
-
-			tmux new-window -n share -t mydev
-			tmux send-keys -t mydev:8 "cd ~/" C-m
-
-			tmux select-window -t mydev:1
-			tmux select-pane -t mydev:1
-		fi
-		tmux attach -t mydev
-
+	elif [ $# == 1 ] && [ $@ == 'my' ]; then
+		tmux_mydev
+	elif  [ $# == 1 ] && [ $@ == 'w' ]; then
+		tmux_company
 	fi
 
 fi
-
