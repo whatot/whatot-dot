4144f0018eff82920da62986becfdd7a8544737c
whatot <whatot2@gmail.com>
2016-07-16 16:06:25 +0800

    replace target_user with sudoer_user in ansible

--- ansible/deploy-ansible.sh
+++ ansible/deploy-ansible.sh
@@ -4,4 +4,4 @@ set -x
 sudo sed -i '/ustc/s/^#//' /etc/pacman.d/mirrorlist
 sudo pacman -S ansible python2
 
-ansible-playbook ansible.yml --extra-vars "target_user=mir"
+ansible-playbook ansible.yml --extra-vars "sudoer_user=mir remote_user=mir"
