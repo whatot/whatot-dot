6ca69ea0a0b6e2c8da806329fb78e75859baf542
whatot <whatot2@gmail.com>
2016-08-02 22:44:14 +0800

    split original ansible.yml into serveral files

--- ansible/config-arch.sh
+++ ansible/config-arch.sh
@@ -4,4 +4,4 @@ set -x
 sudo sed -i '/ustc/s/^#//' /etc/pacman.d/mirrorlist
 sudo pacman -S ansible python2
 
-ansible-playbook ansible.yml --extra-vars "sudoer_user=$USER remote_user=$USER"
+ansible-playbook arch.yml --extra-vars "default_user=$USER remote_user=$USER"
---
+++ ansible/roles/arch/README.md
@@ -0,0 +1,18 @@
+Arch
+=========
+
+Archlinux or Manjaro configuration
+
+Example Playbook
+----------------
+
+```
+- hosts: servers
+  roles:
+      - role: arch
+```
+
+Author Information
+------------------
+
+whatot whatot2@gmail.com
