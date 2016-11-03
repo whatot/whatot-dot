8a71f9b23dacfaab028803858c627f831b845010
whatot <whatot2@gmail.com>
2016-11-03 15:20:50 +0800

    replace oracle with goru

--- deploy-dev-about.sh
+++ deploy-dev-about.sh
@@ -4,12 +4,12 @@ set -x
 # for go
 go get -u -v github.com/nsf/gocode
 go get -u -v github.com/rogpeppe/godef
-go get -u -v golang.org/x/tools/cmd/oracle
+go get -u -v golang.org/x/tools/cmd/guru
 go get -u -v golang.org/x/tools/cmd/gorename
 go get -u -v golang.org/x/tools/cmd/goimports
 go get -u -v github.com/alecthomas/gometalinter
 # gometalinter install all known linters:
-gometalinter --install
+gometalinter --install --update
 
 # for rust
 cargo install rustfmt
