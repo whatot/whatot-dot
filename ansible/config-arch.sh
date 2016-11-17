11ac895f1eedfb45c5a8ba09d4e35957a5f5ced1
whatot <whatot2@gmail.com>
2016-11-17 20:33:41 +0800

    add htop,ydcv,fcitx-about-cmd in role/arch

--- ansible/config-arch.sh
+++ ansible/config-arch.sh
@@ -5,3 +5,10 @@ sudo sed -i '/ustc/s/^#//' /etc/pacman.d/mirrorlist
 sudo pacman -S ansible python2
 
 ansible-playbook arch.yml --extra-vars "default_user=$USER remote_user=$USER"
+
+echo "fcitx-setup for gnome3"
+cat << EndOfMessage
+gsettings set \\
+  org.gnome.settings-daemon.plugins.xsettings overrides \\
+  "{'Gtk/IMModule':<'fcitx'>}"
+EndOfMessage
