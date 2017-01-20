065be8a2616124f78f29a4878c671e65f5492aab
whatot <whatot2@gmail.com>
2017-01-20 17:00:26 +0800

    add imenu-list in spacemacs for tagbar in vim

--- spacemacs.el
+++ spacemacs.el
@@ -78,6 +78,7 @@ values."
      (haskell :variables
               haskell-completion-backend 'ghc-mod
               haskell-enable-hindent-style "johan-tibell")
+     imenu-list
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
