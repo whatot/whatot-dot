b12b7908d5dcf5699e0a5f0ced78691be39ccb44
whatot <whatot2@gmail.com>
2017-01-23 11:45:29 +0800

    add docker support

--- spacemacs.el
+++ spacemacs.el
@@ -79,6 +79,7 @@ values."
               haskell-completion-backend 'ghc-mod
               haskell-enable-hindent-style "johan-tibell")
      imenu-list
+     docker
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
