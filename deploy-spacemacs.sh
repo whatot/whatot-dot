cb033333bd549c686a36f0cccc733479c3ca9421
whatot <whatot2@gmail.com>
2018-04-06 17:07:31 +0800

    avoid lint warning in deploy-*.sh

--- deploy-spacemacs.sh
+++ deploy-spacemacs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -x
 
-PATH_NOW=`pwd`
+PATH_NOW=$(pwd)
 
 if [[ -f "${HOME}/.spacemacs" ]]; then
     mv ~/.spacemacs ~/.spacemacs.bak
 fi
 
-ln -s ${PATH_NOW}/spacemacs.el ~/.spacemacs
+ln -s "${PATH_NOW}"/spacemacs.el ~/.spacemacs
 
 sudo bash -c 'cat > /usr/bin/emacs.wrapper' <<EOF
 #! /bin/bash
--- deploy-ycm-vim.sh
+++ deploy-ycm-vim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-PATH_NOW=`pwd`
+PATH_NOW=$(pwd)
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -12,7 +12,7 @@ fi
 
 # all needed subdirs
 mkdir -p ~/.vim/{plugged,sessions,undodir,autoload}
-ln -s ${PATH_NOW}/ycm.vim ~/.vimrc
+ln -s "${PATH_NOW}"/ycm.vim ~/.vimrc
 
 # Using vim-plug to install plugins in github
 vim +PlugInstall +qall
