2273f4de226c754017dc2f600928a7663f4e2308
whatot <whatot2@gmail.com>
2018-04-14 12:41:03 +0800

    add spacemacs git addr in deploy script

--- deploy-spacemacs.sh
+++ deploy-spacemacs.sh
@@ -15,3 +15,11 @@ export LC_CTYPE=zh_CN.UTF-8; emacsclient -c -a emacs
 EOF
 
 sudo chmod +x /usr/bin/emacs.wrapper
+
+EMACS_D_PATH="${HOME}/.emacs.d"
+if [[ ! -d "${EMACS_D_PATH}" ]];then
+    git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+fi
+
+cd "${EMACS_D_PATH}" || return
+git checkout develop
