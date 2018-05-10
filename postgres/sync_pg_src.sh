bbaef662e0d59516ea59b8dabf11b301b5dc3cde
whatot <whatot2@gmail.com>
2018-05-10 20:15:22 +0800

    fix and improve postgres/sync_pg_src.sh

--- postgres/sync_pg_src.sh
+++ postgres/sync_pg_src.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 set -x
 
-SCRIPT_DIR=`pwd`
+SCRIPT_DIR=$(pwd)
 SHM_DIR=/dev/shm
 SOURCE_DIR=~/git/postgres
 TARGET_DIR=${SHM_DIR}/postgres
 INSTALL_DIR=${SHM_DIR}/pginstall
 DATA_DIR=${SHM_DIR}/pgdata
 
-git -C ~/git/postgres clean -fxd
+git -C "${SOURCE_DIR}" clean -fxd
 mkdir ${TARGET_DIR}
 mkdir ${INSTALL_DIR}
 mkdir ${DATA_DIR}
 
-rsync -avP --delete ~/git/postgres ${SHM_DIR}
-cp ${SCRIPT_DIR}/clang_complete.postgres ${TARGET_DIR}/.clang_complete
-cp ${SCRIPT_DIR}/ycm_extra_conf.py ${TARGET_DIR}/.ycm_extra_conf.py
-cp ${SCRIPT_DIR}/gene-gtags.sh ${TARGET_DIR}/gene-gtags.sh
-cp ${SCRIPT_DIR}/postgres.dir-locals.el ${TARGET_DIR}/.dir-locals.el
+rsync -avP --delete "${SOURCE_DIR}" "${SHM_DIR}"
+cp "${SCRIPT_DIR}"/clang_complete.postgres "${TARGET_DIR}"/.clang_complete
+cp "${SCRIPT_DIR}"/ycm_extra_conf.py "${TARGET_DIR}"/.ycm_extra_conf.py
+cp "${SCRIPT_DIR}"/gen-gtags.sh "${TARGET_DIR}"/gen-gtags.sh
+cp "${SCRIPT_DIR}"/postgres.dir-locals.el "${TARGET_DIR}"/.dir-locals.el
 
-cd ${TARGET_DIR}
-git checkout REL9_6_STABLE
+cd ${TARGET_DIR} || return
+git checkout REL_10_STABLE
 ./configure --prefix=${INSTALL_DIR} \
     --enable-debug \
     --sysconfdir=/etc \
@@ -39,4 +39,4 @@ git checkout REL9_6_STABLE
 make install
 make clean
 make submake-generated-headers
-bash ./gene-gtags.sh
+bash ./gen-gtags.sh
