62291d96ace7d14fd969fcee80ab6ba317adbf30
whatot <whatot2@gmail.com>
2018-05-10 18:19:11 +0800

    maximize windows at startup

--- emacs/doom-private-config.el
+++ emacs/doom-private-config.el
@@ -1,14 +1,18 @@
 
-;;; basic part
+;;; font
 (setq doom-font (font-spec
                  :family "Source Code Pro"
                  :size (cond
                         ((string= (system-name) "x411") '19)
                         ((string= (system-name) "b150") '30)
                         ((string= system-type "darwin") '13))))
+;;; windows frame title
 (setq frame-title-format
           '(buffer-file-name "%f [%m]"
             (dired-directory dired-directory "%b")))
+
+;;; maximized at startup
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
 (provide 'doom-private-config)
 ;;; doom-private-config.el ends here
