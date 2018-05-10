d9e6290eb55108baf15dee9059eff819d3e6cc3a
whatot <whatot2@gmail.com>
2018-05-10 20:00:49 +0800

    quota to avoid shell warning

--- emacs/create-emacs-wrapper.sh
+++ emacs/create-emacs-wrapper.sh
@@ -5,12 +5,11 @@ EMACS_WRAPPER_PATH="/usr/bin/emacs.wrapper"
 
 if [[ ! -f "${EMACS_WRAPPER_PATH}" ]]; then
 
-sudo bash -c 'cat > ${EMACS_WRAPPER_PATH}' <<EOF
+    sudo bash -c 'cat > ${EMACS_WRAPPER_PATH}' <<EOF
 #! /bin/bash
 export LC_CTYPE=zh_CN.UTF-8; emacsclient -c -a emacs
 EOF
 
-sudo chmod +x ${EMACS_WRAPPER_PATH}
+    sudo chmod +x "${EMACS_WRAPPER_PATH}"
 fi
-
 
--- emacs/deploy-doom.sh
+++ emacs/deploy-doom.sh
@@ -8,8 +8,8 @@ PRIVATE_CONFIG_PATH="${HOME}/.emacs.d/modules/config/private"
 PRIVATE_CONFIG_FILE="${PRIVATE_CONFIG_PATH}/config.el"
 
 if [[ ! -d "${DOOM_D_PATH}" ]]; then
-    mkdir -p ${DOOM_D_PATH}
-    ln -s ${PATH_NOW}/doom.el ${DOOM_D_PATH}/init.el
+    mkdir -p "${DOOM_D_PATH}"
+    ln -s "${PATH_NOW}"/doom.el "${DOOM_D_PATH}"/init.el
 fi
 
 if [[ ! -d "${EMACS_D_PATH}" ]]; then
@@ -17,8 +17,8 @@ if [[ ! -d "${EMACS_D_PATH}" ]]; then
 fi
 
 if [[ ! -d "${PRIVATE_CONFIG_PATH}" ]]; then
-    mkdir -p ${PRIVATE_CONFIG_PATH}
-    ln -s ${PATH_NOW}/doom-private-config.el ${PRIVATE_CONFIG_FILE}
+    mkdir -p "${PRIVATE_CONFIG_PATH}"
+    ln -s "${PATH_NOW}"/doom-private-config.el "${PRIVATE_CONFIG_FILE}"
 fi
 
 cd "${EMACS_D_PATH}" || return
