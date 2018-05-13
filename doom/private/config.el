cfea48654bf988be27549642c4b1e12b8a0b8191
whatot <whatot2@gmail.com>
2018-05-13 22:33:22 +0800

    use fuzzy-match in ivy except swiper

--- doom/private/config.el
+++ doom/private/config.el
@@ -24,3 +24,7 @@
 (def-package! youdao-dictionary
   :commands (youdao-dictionary-search-at-point)
   :config (map! "C-c y" 'youdao-dictionary-search-at-point))
+
+(setq ivy-re-builders-alist
+      '((swiper . ivy--regex-plus)
+        (t      . ivy--regex-fuzzy)))
