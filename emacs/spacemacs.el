c6b22ea9739efc70d1e48aad18a066b15b1bcf34
whatot <whatot2@gmail.com>
2018-05-28 21:43:51 +0800

    use doom-one as default theme in spacemacs

--- emacs/spacemacs.el
+++ emacs/spacemacs.el
@@ -102,8 +102,6 @@ This function should only modify configuration layer settings."
      ag
      thrift
      doom-themes
-     ;; zenburn-theme
-     ;; darktooth-theme
      )
 
    ;; A list of packages that cannot be updated.
@@ -227,9 +225,8 @@ It should only modify the values of Spacemacs settings."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(doom-vibrant
-                         ;; darktooth
-                         ;; zenburn
+   dotspacemacs-themes '(doom-one
+                         doom-vibrant
                          spacemacs-dark
                          spacemacs-light)
 
