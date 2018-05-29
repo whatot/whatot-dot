499c0325f7a82173e91654fff02e3f8048be2f1b
whatot <whatot2@gmail.com>
2018-05-29 23:44:43 +0800

    install oh-my-zsh when not in arch or manjaro

--- zsh/deploy-zshrc.sh
+++ zsh/deploy-zshrc.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -x
 
+if [[ ! -d "/usr/share/oh-my-zsh/" ]]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
 SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
 
 ln -sf "${SCRIPT_PATH}"/zshrc ~/.zshrc
