f6e069874913fff9b01aeba0fbd84c1353f4d2cc
whatot <whatot2@gmail.com>
2018-06-26 14:29:31 +0800

    remove useless osx layer

--- emacs/spacemacs.el
+++ emacs/spacemacs.el
@@ -77,7 +77,6 @@ This function should only modify configuration layer settings."
      (rust :variables rust-format-on-save t)
      protobuf
      json
-     (osx :variables osx-use-dictionary-app nil)
      )
 
    ;; List of additional packages that will be installed without being
