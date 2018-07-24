1564b9df58f1d17f723cb83f79de04d7d2507b86
whatot <whatot2@gmail.com>
2018-07-24 15:21:25 +0800

    add dracula theme for spacemacs

--- emacs/spacemacs.el
+++ emacs/spacemacs.el
@@ -92,6 +92,7 @@ This function should only modify configuration layer settings."
      ag
      thrift
      doom-themes
+     dracula-theme
      )
 
    ;; A list of packages that cannot be updated.
@@ -219,7 +220,8 @@ It should only modify the values of Spacemacs settings."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(doom-one
+   dotspacemacs-themes '(dracula
+                         doom-one
                          doom-vibrant
                          spacemacs-dark
                          spacemacs-light)
