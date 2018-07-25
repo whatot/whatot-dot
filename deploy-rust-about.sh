6b231e5ccea5fb6fc37cec6b3e375ae04b64255b
whatot <whatot2@gmail.com>
2018-07-25 11:52:33 +0800

    install clippy and llvm-tools via rustup

--- deploy-rust-about.sh
+++ deploy-rust-about.sh
@@ -13,14 +13,15 @@ EOF
 
 rustup toolchain install nightly
 rustup default nightly
+rustup component add clippy-preview
+rustup component add llvm-tools-preview
 rustup component add rls-preview
+rustup component add rust-analysis
 rustup component add rust-src
 rustup component add rustfmt-preview
-rustup component add rust-analysis
 rustup update
 rustc --print sysroot
 cargo install --force racer
-cargo install --force clippy
 
 # for tldr to update cache, ``tldr --update``
 # cargo install tealdeer
