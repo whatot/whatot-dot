73327d0564c2be7d5c9217afc5b72189bc609fbe
whatot <whatot2@gmail.com>
2018-07-27 22:54:40 +0800

    support msys in deploy-rust-about.sh

--- deploy-rust-about.sh
+++ deploy-rust-about.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -eux
 
-CARGO_CONFIG_FILE="${HOME}/.cargo/config"
+if [[ "$OSTYPE" == "msys" ]]; then
+    CARGO_CONFIG_FILE="$USERPROFILE/.cargo/config"
+else
+    CARGO_CONFIG_FILE="${HOME}/.cargo/config"
+fi
 
 cat <<EOF > "${CARGO_CONFIG_FILE}"
 [source.crates-io]
@@ -15,13 +19,16 @@ rustup toolchain install nightly
 rustup default nightly
 rustup component add clippy-preview
 rustup component add llvm-tools-preview
-rustup component add rls-preview
 rustup component add rust-analysis
 rustup component add rust-src
 rustup component add rustfmt-preview
 rustup update
 rustc --print sysroot
 cargo install --force racer
+
+if [[ ! "$OSTYPE" == "msys" ]]; then
+    rustup component add rls-preview
+fi
 
 # for tldr to update cache, ``tldr --update``
 # cargo install tealdeer
