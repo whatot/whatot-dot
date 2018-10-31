3bae4eb5164167cfb53343193d66bc8feb570576
whatot <whatot2@gmail.com>
2018-10-31 16:05:38 +0800

    remove tabbar for doom-emacs

--- doom/init.el
+++ doom/init.el
@@ -40,7 +40,7 @@
         +all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
        ;;pretty-code       ; replace bits of code with pretty symbols
-       tabbar            ; FIXME an (incomplete) tab bar for Emacs
+       ;;tabbar            ; FIXME an (incomplete) tab bar for Emacs
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
