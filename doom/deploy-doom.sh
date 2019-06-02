36081e9f75a3bd366e7f959f39a1d48b2e0b8d29
whatot <whatot2@gmail.com>
2019-06-02 13:27:24 +0800

    update deploy scripts for doom and spacemacs

--- doom/deploy-doom.sh
+++ doom/deploy-doom.sh
@@ -6,6 +6,11 @@ DOOM_SOURCE_PATH="${HOME}/.emacs.d.doom"
 DOOM_CONFIG_PATH="${HOME}/.doom.d"
 EMACS_D_PATH="${HOME}/.emacs.d"
 
+# 将doom配置指向正确位置
+if [[ ! -d "${DOOM_CONFIG_PATH}" ]]; then
+    ln -sf "${SCRIPT_PATH}" "${DOOM_CONFIG_PATH}"
+fi
+
 # clone源码
 if [[ ! -d "${DOOM_SOURCE_PATH}" ]]; then
     git clone https://github.com/hlissner/doom-emacs "${DOOM_SOURCE_PATH}"
@@ -19,11 +24,7 @@ elif [[ -L "${EMACS_D_PATH}" && -d "${EMACS_D_PATH}" ]]; then
     ln -s "${DOOM_SOURCE_PATH}" "${EMACS_D_PATH}"
 fi
 
-# 将doom配置指向正确位置
-if [[ ! -d "${DOOM_CONFIG_PATH}" ]]; then
-    ln -sf "${SCRIPT_PATH}" "${DOOM_CONFIG_PATH}"
-fi
-
+# 切换分支，更新操作
 cd "${EMACS_D_PATH}" || return
 git checkout develop
 git pull
--- emacs/deploy-spacemacs.sh
+++ emacs/deploy-spacemacs.sh
@@ -2,9 +2,13 @@
 set -x
 
 SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
+SPACEMACS_SRC_PATH="${HOME}/.emacs.d.spacemacs"
+EMACS_D_PATH="${HOME}/.emacs.d"
+
+# 将配置文件指向正确位置
 ln -sf "${SCRIPT_PATH}"/spacemacs.el ~/.spacemacs
 
-# generate ~/.spacemacs.env based on zshenv result
+# 根据环境变量生成~/.spacemacs.env
 bash -c 'cat > ~/.spacemacs.env' <<EOF
 PATH=${PATH}
 GOPATH=${GOPATH}
@@ -13,10 +17,20 @@ RUST_SRC_PATH=${RUST_SRC_PATH}
 XDG_CONFIG_HOME=${HOME}/.config/
 EOF
 
-EMACS_D_PATH="${HOME}/.emacs.d"
-if [[ ! -d "${EMACS_D_PATH}" ]];then
-    git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+# clone源码
+if [[ ! -d "${SPACEMACS_SRC_PATH}" ]];then
+    git clone https://github.com/syl20bnr/spacemacs "${SPACEMACS_SRC_PATH}"
 fi
 
+# 将.emacs.d指向spacemacs源码
+if [[ ! -d "${EMACS_D_PATH}" ]]; then
+    ln -s "${SPACEMACS_SRC_PATH}" "${EMACS_D_PATH}"
+elif [[ -L "${EMACS_D_PATH}" && -d "${EMACS_D_PATH}" ]]; then
+    rm "${EMACS_D_PATH}"
+    ln -s "${SPACEMACS_SRC_PATH}" "${EMACS_D_PATH}"
+fi
+
+# 切换分支，更新操作
 cd "${EMACS_D_PATH}" || return
 git checkout develop
+git pull
