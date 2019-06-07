4ec37951f850148cd9defe38516493c2d9fd9492
whatot <whatot2@gmail.com>
2019-06-07 19:54:10 +0800

    just deploy emacs-snapshot in config-ubuntu.sh

--- ansible/config-ubuntu.sh
+++ ansible/config-ubuntu.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -eux
 
+# https://launchpad.net/~ubuntu-elisp/+archive/ubuntu/ppa
+sudo add-apt-repository ppa:ubuntu-elisp/ppa
+
 sudo apt update
 sudo apt install vim zsh git curl zsh openssh-server htop
 sudo apt install libssl-dev build-essential pkg-config zlib1g-dev cmake
+
+sudo apt install emacs-snapshot
--- emacs/install-emacs-ubuntu.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-
-# https://launchpad.net/~ubuntu-elisp/+archive/ubuntu/ppa
-
-sudo add-apt-repository ppa:ubuntu-elisp/ppa
-sudo apt-get update
-sudo apt install emacs-snapshot
-
