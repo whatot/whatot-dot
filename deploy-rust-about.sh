5dd73f0c672da57d97376e5269fb5ca1ee63eb7a
whatot <whatot2@gmail.com>
2019-07-13 09:35:57 +0800

    use 0711 for rust nightly

--- deploy-rust-about.sh
+++ deploy-rust-about.sh
@@ -31,7 +31,7 @@ rustup component add rustfmt --toolchain "${NEEDED_RUST_VERSION}"
 
 # 最新的健康nightly版本
 echo
-HEALTHY_NIGHTLY_VERSION=nightly-2019-06-28
+HEALTHY_NIGHTLY_VERSION=nightly-2019-07-11
 rustup toolchain install "${HEALTHY_NIGHTLY_VERSION}"
 rustup default "${HEALTHY_NIGHTLY_VERSION}"
 rustup component add rls
