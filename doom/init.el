2fc0615e2809fa5d51a78a405a41d4d30c0da54d
whatot <whatot2@gmail.com>
2019-09-10 15:57:14 +0800

    add go lsp support in doom el

--- doom/init.el
+++ doom/init.el
@@ -109,7 +109,8 @@
        emacs-lisp        ; drown in parentheses
        ;;ess               ; emacs speaks statistics
        ;;fsharp           ; ML stands for Microsoft's Language
-       ;;go                ; the hipster dialect
+       (go                ; the hipster dialect
+        +lsp)
        (haskell +intero) ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
