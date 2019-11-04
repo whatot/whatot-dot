f0dd9b2666231891b50201123ed6e17c4e39d437
whatot <whatot2@gmail.com>
2019-11-04 18:13:13 +0800

    support go in spacemacs

--- emacs/spacemacs.el
+++ emacs/spacemacs.el
@@ -82,6 +82,7 @@ This function should only modify configuration layer settings."
      json
      plantuml
      neotree
+     go
      )
 
    ;; List of additional packages that will be installed without being
