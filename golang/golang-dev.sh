16faad79000a75f72ce69d4c9738884721d3c3c8
whatot <whatot2@gmail.com>
2020-02-14 22:23:04 +0800

    install golangci-lint by binary

--- golang/golang-dev.sh
+++ golang/golang-dev.sh
@@ -10,10 +10,21 @@ go get -u -v github.com/cweill/gotests/...
 go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
 go get -u -v github.com/fatih/gomodifytags
 go get -u -v github.com/godoctor/godoctor
-go get -u -v github.com/golangci/golangci-lint/cmd/golangci-lint
 go get -u -v github.com/haya14busa/gopkgs/cmd/gopkgs
 go get -u -v github.com/josharian/impl
 go get -u -v github.com/mdempsky/gocode
 go get -u -v github.com/rogpeppe/godef
 go get -u -v github.com/zmb3/gogetdoc
 
+
+# https://github.com/golangci/golangci-lint#install
+# suggest install by binary(brew) rather than go-get
+# go get -u -v github.com/golangci/golangci-lint/cmd/golangci-lint
+case $(uname) in
+    Darwin)
+        brew install golangci/tap/golangci-lint
+    ;;
+    Linux)
+        yaourt -S golangci-lint
+    ;;
+esac
