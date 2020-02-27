7f0d580957543f0d89dc656573ef8d3ad114a319
whatot <whatot2@gmail.com>
2020-02-27 16:15:56 +0800

    use alt-tab to switch apps only in workspace

--- ansible/config-arch.sh
+++ ansible/config-arch.sh
@@ -11,6 +11,8 @@ time ansible-playbook arch.yml -i hosts.yml --extra-vars "default_user=$USER"
 
 echo "fcitx-setup for gnome3"
 cat << EOF
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
+
 gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gtk/IMModule':<'fcitx'>}"
 
 gsettings set org.gnome.shell.keybindings switch-to-application-1 "[]"
