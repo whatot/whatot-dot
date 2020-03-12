9980adb733b8893b68c56be7fa19529fdb30725c
whatot <whatot2@gmail.com>
2020-03-12 11:20:52 +0800

    support retry in upgrade_git.py

--- git/upgrade_git.py
+++ git/upgrade_git.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 """
 Upgrade the git repos in current dir to latest. Error repos log into error.log.
-2016-01-24, whatot
+2016-01-24, init
+2020-03-12, retry
 """
 
 import os
@@ -10,16 +11,21 @@ import threading
 import logging
 import subprocess
 
-
-G_QUEUE = queue.Queue()
-G_WORKER_THREAD_NUM = 8
-G_ROOTPATH = os.getcwd()
-GIT_UPGRADE_CMD = "git pull && git submodule update --init --recursive"
+WORK_THREAD_NUM = 4
+MAX_RETRY_COUNT = 3
+GIT_UPGRADE_CMD = "git pull && git submodule update --init --recursive && git gc"
 HG_UPGRADE_CMD = "hg pull && hg update"
+
+logging.basicConfig(
+    filename="error.log",
+    level=logging.ERROR,
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+)
 
 
 class WorkerThread(threading.Thread):
     """main work thread process"""
+
     def __init__(self, t_name, t_queue):
         threading.Thread.__init__(self, name=t_name)
         self.queue = t_queue
@@ -27,14 +33,22 @@ class WorkerThread(threading.Thread):
     def run(self):
         while True:
             item = self.queue.get()
-            run_cmd = "cd " + item[0] + " && " + item[1]
-            print_header = ">>> update" + item[0] + "\n"
+            path = item[0]
+            cmd = item[1]
+            retry_count = item[2]
+            run_cmd = "cd " + path + " && " + cmd
+            print_header = ">>> update " + path + "\n"
+
+            if retry_count > MAX_RETRY_COUNT:
+                logging.error("%s end retry: %d", path, retry_count)
+                continue
+
             try:
                 out = subprocess.check_output(run_cmd, shell=True)
                 print(print_header + out.decode())
             except subprocess.CalledProcessError as errorexception:
                 print(print_header + errorexception.output.decode())
-                logging.error(item[0])
+                logging.error("%s failed retry: %d", path, retry_count)
 
             self.queue.task_done()
 
@@ -42,36 +56,34 @@ class WorkerThread(threading.Thread):
                 break
 
 
-def put_task():
+def put_task(root_path, work_queue):
     """put all the repos in current dir into global task Queue: G_QUEUE."""
-    for path in os.listdir(G_ROOTPATH):
-        fullpath = os.path.join(G_ROOTPATH, path)
+    for path in os.listdir(root_path):
+        fullpath = os.path.join(root_path, path)
         if os.path.isdir(fullpath):
-            if os.path.isdir(os.path.join(fullpath, '.git')):
-                G_QUEUE.put([fullpath, GIT_UPGRADE_CMD])
-            elif os.path.isdir(os.path.join(fullpath, '.hg')):
-                G_QUEUE.put([fullpath, HG_UPGRADE_CMD])
+            if os.path.isdir(os.path.join(fullpath, ".git")):
+                work_queue.put([fullpath, GIT_UPGRADE_CMD, 0])
+            elif os.path.isdir(os.path.join(fullpath, ".hg")):
+                work_queue.put([fullpath, HG_UPGRADE_CMD, 0])
 
 
 def main():
-    """error into error.log, start threads"""
-    logformat = '%(asctime)s - %(levelname)s - %(name)s - %(message)s'
-    logging.basicConfig(filename='error.log',
-                        level=logging.ERROR,
-                        format=logformat)
+    """init queue, then start threads"""
+    root_path = os.getcwd()
+    work_queue = queue.Queue()
     threads = []
-    put_task()
+    put_task(root_path, work_queue)
 
-    for i in range(G_WORKER_THREAD_NUM):
-        thread = WorkerThread("WorkerThread" + str(i), G_QUEUE)
+    for i in range(WORK_THREAD_NUM):
+        thread = WorkerThread("WorkerThread" + str(i), work_queue)
         thread.start()
         threads.append(thread)
     for thread in threads:
         thread.join()
 
-    G_QUEUE.join()
+    work_queue.join()
     print("All finished!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
