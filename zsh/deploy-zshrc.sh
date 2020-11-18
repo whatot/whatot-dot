8e6cb4e758ed2a40ef7637fdfff5b2366021b2f5
--global <--global>
2020-11-19 02:17:39 +0800

    update zsh url and wsl proxy config

--- zsh/deploy-zshrc.sh
+++ zsh/deploy-zshrc.sh
@@ -2,7 +2,7 @@
 set -x
 
 if [[ ! -d "/usr/share/oh-my-zsh/" ]]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
