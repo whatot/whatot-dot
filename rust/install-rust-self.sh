71fc06bc46fd6ec4c549905ad32db19818776243
whatot <whatot2@gmail.com>
2021-01-23 22:50:19 +0800

    use rust nightly-2021-01-22

--- rust/install-rust-self.sh
+++ rust/install-rust-self.sh
@@ -31,7 +31,7 @@ rustup component add rustfmt --toolchain "${NEEDED_RUST_VERSION}"
 
 # 最新的健康nightly版本
 echo
-HEALTHY_NIGHTLY_VERSION=nightly-2020-12-27
+HEALTHY_NIGHTLY_VERSION=nightly-2021-01-22
 rustup toolchain install "${HEALTHY_NIGHTLY_VERSION}"
 rustup default "${HEALTHY_NIGHTLY_VERSION}"
 rustup component add rls
