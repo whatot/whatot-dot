ac5bf5257ebd4dd13e196bc4210dae745777bbc0
whatot <whatot2@gmail.com>
2021-02-02 00:25:51 +0800

    install rust-analyzer-preview by default

--- rust/install-rust-self.sh
+++ rust/install-rust-self.sh
@@ -20,23 +20,29 @@ registry = "git://mirrors.ustc.edu.cn/crates.io-index"
 registry = "https://mirrors.tuna.tsinghua.edu.cn/git/crates.io-index.git"
 EOF
 
+
 # 特殊指定版本
 NEEDED_RUST_VERSION=nightly-2020-10-19
 rustup toolchain install "${NEEDED_RUST_VERSION}"
-rustup component add rls --toolchain "${NEEDED_RUST_VERSION}"
 rustup component add clippy --toolchain "${NEEDED_RUST_VERSION}"
-rustup component add rust-analysis --toolchain "${NEEDED_RUST_VERSION}"
+rustup component add rust-analyzer-preview --toolchain "${NEEDED_RUST_VERSION}"
 rustup component add rust-src --toolchain "${NEEDED_RUST_VERSION}"
 rustup component add rustfmt --toolchain "${NEEDED_RUST_VERSION}"
-
-# 最新的健康nightly版本
 echo
+
+# 最新的健康nightly版本,临时自己加脚本解决调用问题
 HEALTHY_NIGHTLY_VERSION=nightly-2021-01-22
+RUST_ANALYZER_BIN="${HOME}/.cargo/bin/rust-analyzer"
+cat <<EOF > "${RUST_ANALYZER_BIN}"
+#!/bin/sh
+rustup run ${HEALTHY_NIGHTLY_VERSION} rust-analyzer "\$@"
+EOF
+chmod +x "${RUST_ANALYZER_BIN}"
+
 rustup toolchain install "${HEALTHY_NIGHTLY_VERSION}"
 rustup default "${HEALTHY_NIGHTLY_VERSION}"
-rustup component add rls
 rustup component add clippy
-rustup component add rust-analysis
+rustup component add rust-analyzer-preview
 rustup component add rust-src
 rustup component add rustfmt
 rustc --print sysroot
