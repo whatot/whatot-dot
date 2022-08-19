699f8fd4708b865ff20f4ff6439f5390901d372c
whatot <whatot2@gmail.com>
2022-08-19 17:01:09 +0800

    feat: use go install rather than go get

--- golang/golang-dev.sh
+++ golang/golang-dev.sh
@@ -2,20 +2,16 @@
 set -eux
 
 function common_pkgs_by_go() {
-    go get -u -v github.com/cweill/gotests/...
-    go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
-    go get -u -v github.com/fatih/gomodifytags
-    go get -u -v github.com/godoctor/godoctor
-    go get -u -v github.com/haya14busa/gopkgs/cmd/gopkgs
-    go get -u -v github.com/josharian/impl
-    go get -u -v github.com/mdempsky/gocode
-    go get -u -v github.com/rogpeppe/godef
-    go get -u -v github.com/zmb3/gogetdoc
-    go get -u -v github.com/motemen/gore/cmd/gore
-
-    # don't use -u when install gopls
-    rm -f ~/go/bin/gopls
-    GO111MODULE=on go get -v golang.org/x/tools/gopls@latest
+    go install github.com/davidrjenni/reftools/cmd/fillstruct@latest
+    go install github.com/fatih/gomodifytags@latest
+    go install github.com/godoctor/godoctor@latest
+    go install github.com/haya14busa/gopkgs/cmd/gopkgs@latest
+    go install github.com/josharian/impl@latest
+    go install github.com/mdempsky/gocode@latest
+    go install github.com/rogpeppe/godef@latest
+    go install github.com/zmb3/gogetdoc@latest
+    go install github.com/x-motemen/gore/cmd/gore@latest
+    go install golang.org/x/tools/gopls@latest
 }
 
 function for_linux() {
@@ -29,13 +25,15 @@ function for_mac() {
     # suggest install by binary(brew)
     brew install golangci-lint
 
-    go get -u -v golang.org/x/tools/cmd/goimports
-    go get -u -v golang.org/x/tools/cmd/godoc
-    go get -u -v golang.org/x/tools/cmd/gorename
-    go get -u -v golang.org/x/tools/cmd/guru
+    go install golang.org/x/tools/cmd/goimports@latest
+    go install golang.org/x/tools/cmd/godoc@latest
+    go install golang.org/x/tools/cmd/gorename@latest
+    go install golang.org/x/tools/cmd/guru@latest
 
     common_pkgs_by_go
 }
+
+export GO111MODULE=on
 
 case $(uname) in
     Darwin)
