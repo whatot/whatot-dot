578096928a36fb9f3a0c233865d89ad985d7df94
whatot <whatot2@gmail.com>
2022-08-23 20:50:39 +0800

    feat: support arch,mac,ubuntu in golang-dev.sh

--- golang/golang-dev.sh
+++ golang/golang-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 
-function common_pkgs_by_go() {
+function common_dev_pkgs() {
     go install github.com/davidrjenni/reftools/cmd/fillstruct@latest
     go install github.com/fatih/gomodifytags@latest
     go install github.com/godoctor/godoctor@latest
@@ -14,10 +14,20 @@ function common_pkgs_by_go() {
     go install golang.org/x/tools/gopls@latest
 }
 
-function for_linux() {
+function for_arch() {
     yaourt -S go go-tools golangci-lint-bin
+}
 
-    common_pkgs_by_go
+function for_ubuntu() {
+    if [[ -f "/etc/apt/sources.list.d/longsleep-ubuntu-golang-backports-focal.list" ]]; then
+        echo "golang-backports ppa is already installed"
+    else
+        echo "try to install golang-backports ppa"
+        sudo add-apt-repository ppa:longsleep/golang-backports
+    fi
+
+    sudo apt update
+    sudo apt install golang-go
 }
 
 function for_mac() {
@@ -29,19 +39,31 @@ function for_mac() {
     go install golang.org/x/tools/cmd/godoc@latest
     go install golang.org/x/tools/cmd/gorename@latest
     go install golang.org/x/tools/cmd/guru@latest
-
-    common_pkgs_by_go
 }
 
 export GO111MODULE=on
+OS_ID=$(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release)
 
 case $(uname) in
     Darwin)
         for_mac
     ;;
     Linux)
-        for_linux
+        case ${OS_ID} in
+        ubuntu)
+            for_ubuntu
+        ;;
+        manjaro | archlinux)
+            for_arch
+        ;;
+        *)
+            echo -n "unsupported os id: ${OS_ID}"
+        ;;
+        esac
     ;;
     *)
+        echo -n "unsuppprted os"
     ;;
 esac
+
+common_dev_pkgs
