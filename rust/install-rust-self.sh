51ad6e76463a22af9998cc9f7701d82622d225cb
whatot <whatot2@gmail.com>
2022-12-25 23:36:26 +0800

    feat: init rust-analyzer along with rustup

--- rust/install-rust-self.sh
+++ rust/install-rust-self.sh
@@ -9,7 +9,7 @@ CARGO_CONFIG_PATH="${HOME}/.cargo"
 LOCAL_RUSTUP_BIN="${CARGO_CONFIG_PATH}/bin/rustup"
 mkdir -p "${CARGO_CONFIG_PATH}"
 
-cat <<EOF > "${CARGO_CONFIG_PATH}/config"
+cat <<EOF >"${CARGO_CONFIG_PATH}/config"
 [source.crates-io]
 registry = "https://github.com/rust-lang/crates.io-index"
 replace-with = 'tuna'
@@ -30,25 +30,27 @@ case $(uname) in
             echo -n "try to run rustup-init in macos"
             rustup-init -y --default-toolchain "${NEEDED_RUST_VERSION}" --no-modify-path
         fi
-    ;;
+        # rust-analyzer不住上面的rustup-init软链范围，需要单独处理
+        brew install rust-analyzer
+        ;;
     Linux)
         if [[ -f "/usr/bin/pacman" ]]; then
             # arch 与 manjaro 直接系统安装 rustup，不需要执行安装脚本
-            echo -n "install rustup by pkg manager in arch or manjaro"
-            sudo pacman -S rustup
+            echo -n "install rustup,rust-analyzer by pkg manager in arch or manjaro"
+            sudo pacman -S rustup rust-analyzer
         else
             # 不能直接使用系统 rustup 的发行版中，使用远程安装脚本初始化 rustup
-            if  [[ -f "${LOCAL_RUSTUP_BIN}" ]]; then
+            if [[ -f "${LOCAL_RUSTUP_BIN}" ]]; then
                 echo -n "already init rustup with curl"
             else
                 echo -n "to init rustup with curl"
                 curl https://sh.rustup.rs -sSf | sh
             fi
         fi
-    ;;
+        ;;
     *)
         echo -n "unsuppprted os"
-    ;;
+        ;;
 esac
 
 rustup toolchain install "${NEEDED_RUST_VERSION}"
