2aa32ca7c4f4e0fb18246e3498eba033023765f3
whatot <whatot2@gmail.com>
2023-06-29 23:05:51 +0800

    feat: add helix for ubuntu

--- ansible/config-ubuntu.sh
+++ ansible/config-ubuntu.sh
@@ -4,6 +4,10 @@ set -eux
 # https://launchpad.net/~kelleyk/+archive/ubuntu/emacs
 # sudo add-apt-repository ppa:kelleyk/emacs
 # sudo apt install emacs28-nativecomp
+#
+# https://launchpad.net/~maveonair/+archive/ubuntu/helix-editor
+# sudo add-apt-repository ppa:maveonair/helix-editor
+# sudo apt install helix
 
 sudo apt update
 sudo apt install vim zsh git curl zsh openssh-server htop jq
