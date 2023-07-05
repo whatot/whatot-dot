cb8acf72a208b6c78dcbb4273bf2d32b6dd19a66
whatot <whatot2@gmail.com>
2023-07-05 23:14:10 +0800

    feat: add podman about

--- ansible/config-ubuntu.sh
+++ ansible/config-ubuntu.sh
@@ -12,6 +12,7 @@ set -eux
 sudo apt update
 sudo apt install vim zsh git curl zsh openssh-server htop jq
 sudo apt install libssl-dev build-essential pkg-config zlib1g-dev cmake
+sudo apt install podman podman-docker podman-toolbox
 
 # jq example
 # cat Cargo.lock | toml2json | jq '.package | length'
