6b70043f725170bd00f0f1e9b0426b69cee0c4b4
whatot <whatot2@gmail.com>
2023-07-21 23:16:30 +0800

    feat: add ex function and precmd in starship.zsh

--- zsh/starship.zsh
+++ zsh/starship.zsh
@@ -129,6 +129,32 @@ config_alias() {
 	export EDITOR='vim'
 }
 
+# ex - archive extractor
+# usage: ex <file>
+ex() {
+	if [[ -f $1 ]]; then
+		case $1 in
+			*.tar.bz2) tar xjf $1 ;;
+			*.tar.gz) tar xzf $1 ;;
+			*.bz2) bunzip2 $1 ;;
+			*.rar) unrar x $1 ;;
+			*.gz) gunzip $1 ;;
+			*.tar) tar xf $1 ;;
+			*.tbz2) tar xjf $1 ;;
+			*.tgz) tar xzf $1 ;;
+			*.zip) unzip $1 ;;
+			*.Z) uncompress $1 ;;
+			*.7z) 7z x $1 ;;
+			*) echo "'$1' cannot be extracted via ex()" ;;
+		esac
+	else
+		echo "'$1' is not a valid file"
+	fi
+}
+
+# like PROMPT_COMMAND in bash
+precmd() {print -Pn "\e]0;%n@%m: %~\a"}
+
 # starship only
 eval "$(starship init zsh)"
 
