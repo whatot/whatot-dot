c31b55518353eb9ef25a9087e6f7518de5033e75
whatot <whatot2@gmail.com>
2023-07-26 18:13:25 +0800

    feat: add for nerd font for mac

--- ansible/config-mac.sh
+++ ansible/config-mac.sh
@@ -97,6 +97,8 @@ brew_cask_install iina
 brew_cask_install visual-studio-code
 brew_cask_install macvim
 brew_cask_install meld
+brew_cask_install font-hack-nerd-font
+brew_cask_install font-jetbrains-mono-nerd-font
 brew_cask_install font-meslo-lg-nerd-font
 
 echo "finined!"
