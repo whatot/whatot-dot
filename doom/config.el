465c54e0b123c16c1c04343cd87184d234a8df8a
whatot <whatot2@gmail.com>
2023-07-29 22:18:14 +0800

    feat: adjust font and add roam daily templates

--- doom/config.el
+++ doom/config.el
@@ -9,11 +9,11 @@
 (defvar +prefer-symbol-fonts '("Apple Symbols" "Noto Sans Symbols"))
 (defvar +font-size
   (cl-case (system-name)
-    ("msi" 16)
-    ("gs65" 16)
+    ("msi" 17)
+    ("gs65" 17)
     ("b150" 32)
     ("star" 14)
-    ("mercury" 13)))
+    ("mercury" 14)))
 (defun +match-font-p (font-candidates)
   (cl-loop for font in font-candidates
            when (+font-available-p font)
@@ -72,6 +72,28 @@
       (if (string= (system-name) "star")
           (file-truename "~/roam")
         (file-truename "~/nutstore/thoughts/roam")))
+(setq org-roam-dailies-capture-templates
+      (let ((head "#+title: %<%Y-%m-%d (%A)>\n#+startup: showall\n* [/] Do Today\n* [/] Maybe Do Today\n* Journal\n"))
+        `(("j" "journal" entry
+           #'org-roam-capture--get-point
+           "* %<%H:%M> %?"
+           :file-name "daily/%<%Y-%m-%d>"
+           :head ,head
+           :olp ("Journal"))
+          ("t" "do today" item
+           #'org-roam-capture--get-point
+           "[ ] %(princ as/agenda-captured-link)"
+           :file-name "daily/%<%Y-%m-%d>"
+           :head ,head
+           :olp ("Do Today")
+           :immediate-finish t)
+          ("m" "maybe do today" item
+           #'org-roam-capture--get-point
+           "[ ] %(princ as/agenda-captured-link)"
+           :file-name "daily/%<%Y-%m-%d>"
+           :head ,head
+           :olp ("Maybe Do Today")
+           :immediate-finish t))))
 
 ;; use sqlformat
 (use-package! sqlformat
