18e69fa704a52607fb42749eaf2a981137504046
whatot <whatot2@gmail.com>
2023-08-24 11:11:49 +0800

    feat: lsp use plist in emacs

--- doom/lisp/init-optimize.el
+++ doom/lisp/init-optimize.el
@@ -5,6 +5,10 @@
 ;; 增大同LSP服务器交互时的读取文件的大小 128MB
 (setq read-process-output-max (* 1024 1024 128))
 
+;; https://emacs-lsp.github.io/lsp-mode/page/performance/#use-plists-for-deserialization
+(defvar lsp-use-plists)
+(setq lsp-use-plists t)
+
 (prefer-coding-system 'utf-8)
 
 (provide 'init-optimize)
