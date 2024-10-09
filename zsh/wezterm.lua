d8fc699524fa1ab395d3a83bad3a7954e97a1a75
whatot <whatot2@gmail.com>
2024-10-09 23:47:31 +0800

    feat: short wezterm.lua config

--- zsh/wezterm.lua
+++ zsh/wezterm.lua
@@ -6,7 +6,6 @@ local config = {}
 config.font_size = 13.0
 config.font = wezterm.font_with_fallback {'JetBrains Mono', 'Monaco'}
 config.color_scheme = 'Monokai (terminal.sexy)'
-config.term = "xterm-256color"
 
 -- all window about
 config.window_background_gradient = {
@@ -41,37 +40,13 @@ wezterm.on('gui-startup', function(cmd)
     local tab, pane, window = mux.spawn_window(cmd or {})
     window:gui_window():maximize()
 end)
-wezterm.on("update-right-status", function(window)
-    local date = wezterm.strftime("%Y-%m-%d %H:%M:%S ")
-    window:set_right_status(wezterm.format({{
-        Text = date
-    }}))
-end)
 
 -- all tab about
--- change the title of tab to current working directory.
--- https://wezfurlong.org/wezterm/config/lua/window-events/format-tab-title.html#format-tab-title
--- https://wezfurlong.org/wezterm/config/lua/PaneInformation.html
--- https://wezfurlong.org/wezterm/config/lua/pane/get_current_working_dir.html
-wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
-    local active_pane = tab.active_pane
-
-    -- cwd is a URL object with file:// as beginning.
-    local cwd = active_pane.current_working_dir
-    if cwd == nil then
-        return
-    end
-
-    -- get cwd in string format, https://wezfurlong.org/wezterm/config/lua/wezterm.url/Url.html
-    local cwd_str = cwd.file_path
-
-    -- shorten the path by using ~ as $HOME.
-    local home_dir = os.getenv('HOME')
-    return string.gsub(cwd_str, home_dir, '~')
-end)
 config.enable_tab_bar = true
 config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = false
+config.prefer_to_spawn_tabs = true
+config.tab_max_width = 32
 
 return config
