7cdd135a2b6a29e081101b02aaa8307d02adf0ea
whatot <whatot2@gmail.com>
2024-10-28 01:10:17 +0800

    feat: adjust zsh config order

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,33 +1,17 @@
 # shellcheck disable=SC2148,SC1090,SC1091
-# Start configuration added by Zim install {{{
-#
-# User configuration sourced by interactive shells
-#
 
 # -----------------
-# Zsh configuration
+# common setting
 # -----------------
-
-#
-# History
-#
 
 # Remove older command from the history if a duplicate is to be added.
 setopt HIST_IGNORE_ALL_DUPS
-
-#
-# Input/output
-#
-
 # Set editor default keymap to emacs (`-e`) or vi (`-v`)
 bindkey -e
-
 # Prompt for spelling correction of commands.
-#setopt CORRECT
-
+setopt CORRECT
 # Customize spelling correction prompt.
-#SPROMPT='zsh: correct %F{red}%R%f to %F{green}%r%f [nyae]? '
-
+SPROMPT='zsh: correct %F{red}%R%f to %F{green}%r%f [nyae]? '
 # Remove path separator from WORDCHARS.
 WORDCHARS=${WORDCHARS//[\/]}
 
@@ -42,94 +26,37 @@ WORDCHARS=${WORDCHARS//[\/]}
 # Module configuration
 # --------------------
 
-#
-# git
-#
-
 # Set a custom prefix for the generated aliases. The default prefix is 'G'.
 zstyle ':zim:git' aliases-prefix 'g'
 
-#
-# input
-#
-
 # Append `../` to your input for each `.` you type after an initial `..`
 zstyle ':zim:input' double-dot-expand yes
-
-#
-# termtitle
-#
 
 # Set a custom terminal title format using prompt expansion escape sequences.
 # See http://zsh.sourceforge.net/Doc/Release/Prompt-Expansion.html#Simple-Prompt-Escapes
 # If none is provided, the default '%n@%m: %~' is used.
 #zstyle ':zim:termtitle' format '%1~'
 
-#
 # zsh-autosuggestions
-#
-
 # Disable automatic widget re-binding on each precmd. This can be set when
 # zsh-users/zsh-autosuggestions is the last module in your ~/.zimrc.
 ZSH_AUTOSUGGEST_MANUAL_REBIND=1
-
 # Customize the style that the suggestions are shown with.
 # See https://github.com/zsh-users/zsh-autosuggestions/blob/master/README.md#suggestion-highlight-style
 #ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=242'
 
-#
 # zsh-syntax-highlighting
-#
-
 # Set what highlighters will be used.
 # See https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters.md
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
-
 # Customize the main highlighter styles.
 # See https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters/main.md#how-to-tweak-it
 #typeset -A ZSH_HIGHLIGHT_STYLES
 #ZSH_HIGHLIGHT_STYLES[comment]='fg=242'
 
 # ------------------
-# Initialize modules
+# Config proxy
 # ------------------
-
-ZIM_HOME=${ZDOTDIR:-${HOME}}/.zim
-# Download zimfw plugin manager if missing.
-if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
-  if (( ${+commands[curl]} )); then
-    curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
-        https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
-  else
-    mkdir -p ${ZIM_HOME} && wget -nv -O ${ZIM_HOME}/zimfw.zsh \
-        https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
-  fi
-fi
-# Install missing modules, and update ${ZIM_HOME}/init.zsh if missing or outdated.
-if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZDOTDIR:-${HOME}}/.zimrc ]]; then
-  source ${ZIM_HOME}/zimfw.zsh init -q
-fi
-# Initialize modules.
-source ${ZIM_HOME}/init.zsh
-
-# ------------------------------
-# Post-init module configuration
-# ------------------------------
-
-#
-# zsh-history-substring-search
-#
-
-zmodload -F zsh/terminfo +p:terminfo
-# Bind ^[[A/^[[B manually so up/down works both before and after zle-line-init
-for key ('^[[A' '^P' ${terminfo[kcuu1]}) bindkey ${key} history-substring-search-up
-for key ('^[[B' '^N' ${terminfo[kcud1]}) bindkey ${key} history-substring-search-down
-for key ('k') bindkey -M vicmd ${key} history-substring-search-up
-for key ('j') bindkey -M vicmd ${key} history-substring-search-down
-unset key
-# }}} End configuration added by Zim install
-
-
 load_env_file() {
   file_path=$1
 
@@ -143,7 +70,6 @@ load_env_file() {
     )"
   fi
 }
-
 init_before_all() {
   uname_os=$(uname -s)
   export ZSH_OSTYPE=${uname_os}
@@ -157,7 +83,6 @@ init_before_all() {
 
   load_env_file "${HOME}/.env_private"
 }
-
 unsetproxy() {
   unset NO_PROXY
   unset ALL_PROXY
@@ -189,6 +114,50 @@ proxyinfo() {
   echo "HTTP_PROXY = ${HTTP_PROXY}"
   echo "HTTPS_PROXY = ${HTTPS_PROXY}"
 }
+
+# init env, path about before all
+init_before_all
+# turn on proxy by default
+setproxy
+
+# ------------------
+# Initialize modules
+# ------------------
+
+ZIM_HOME=${ZDOTDIR:-${HOME}}/.zim
+# Download zimfw plugin manager if missing.
+if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
+  if (( ${+commands[curl]} )); then
+    curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
+        https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+  else
+    mkdir -p ${ZIM_HOME} && wget -nv -O ${ZIM_HOME}/zimfw.zsh \
+        https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+  fi
+fi
+# Install missing modules, and update ${ZIM_HOME}/init.zsh if missing or outdated.
+if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZDOTDIR:-${HOME}}/.zimrc ]]; then
+  source ${ZIM_HOME}/zimfw.zsh init -q
+fi
+# Initialize modules.
+source ${ZIM_HOME}/init.zsh
+
+# ------------------------------
+# Post-init module configuration
+# ------------------------------
+
+# zsh-history-substring-search
+zmodload -F zsh/terminfo +p:terminfo
+# Bind ^[[A/^[[B manually so up/down works both before and after zle-line-init
+for key ('^[[A' '^P' ${terminfo[kcuu1]}) bindkey ${key} history-substring-search-up
+for key ('^[[B' '^N' ${terminfo[kcud1]}) bindkey ${key} history-substring-search-down
+for key ('k') bindkey -M vicmd ${key} history-substring-search-up
+for key ('j') bindkey -M vicmd ${key} history-substring-search-down
+unset key
+
+# ------------------------------
+# Other App configuration
+# ------------------------------
 
 config_java_about() {
   # for gradle proxy setting
@@ -267,12 +236,6 @@ config_mixed() {
   ulimit -n 10240
 }
 
-# init env, path about before all
-init_before_all
-
-# turn on proxy by default
-setproxy
-
 # java project
 config_java_about
 
@@ -288,5 +251,5 @@ config_mixed
 # support direnv. add .envrc, then direnv allow.
 eval "$(direnv hook zsh)"
 
-# starship last
+# comment starship and use quick asciiship now
 # eval "$(starship init zsh)"
