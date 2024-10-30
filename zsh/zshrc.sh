de1b32533d67ceeb8b4581c5d93a39b6168895d4
whatot <whatot2@gmail.com>
2024-10-31 00:00:17 +0800

    feat: better term title for running process

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -36,6 +36,9 @@ zstyle ':zim:input' double-dot-expand yes
 # See http://zsh.sourceforge.net/Doc/Release/Prompt-Expansion.html#Simple-Prompt-Escapes
 # If none is provided, the default '%n@%m: %~' is used.
 #zstyle ':zim:termtitle' format '%1~'
+zstyle ':zim:termtitle' hooks 'preexec' 'precmd'
+zstyle ':zim:termtitle:preexec' format '${${(A)=1}[1]}'
+zstyle ':zim:termtitle:precmd'  format '%1~'
 
 # zsh-autosuggestions
 # Disable automatic widget re-binding on each precmd. This can be set when
