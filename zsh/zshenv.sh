1b79e264b3557ffa8d596693470fe097b7e09b7f
whatot <whatot2@gmail.com>
2025-01-20 10:18:51 +0800

    feat: add ndk path check when init android path

--- zsh/zshenv.sh
+++ zsh/zshenv.sh
@@ -1,34 +1,35 @@
 case $(uname) in
-  Darwin)
+Darwin)
     if [[ -f "/opt/homebrew/bin/brew" ]]; then
-      export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:/opt/homebrew/opt/rustup/bin:/opt/homebrew/opt/ccache/libexec:/opt/homebrew/opt/openjdk@21/bin:/opt/homebrew/bin:/opt/homebrew/sbin:${HOME}/.cargo/bin:${HOME}/go/bin:${HOME}/.rd/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
-      export JAVA_HOME="/opt/homebrew/opt/openjdk@21"
+        export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:/opt/homebrew/opt/rustup/bin:/opt/homebrew/opt/ccache/libexec:/opt/homebrew/opt/openjdk@21/bin:/opt/homebrew/bin:/opt/homebrew/sbin:${HOME}/.cargo/bin:${HOME}/go/bin:${HOME}/.rd/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+        export JAVA_HOME="/opt/homebrew/opt/openjdk@21"
     else
-      export PATH="/usr/local/opt/rustup/bin:/opt/local/opt/ccache/libexec:/usr/local/opt/openjdk@21/bin:${HOME}/.cargo/bin:${HOME}/go/bin:${HOME}/.rd/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
-      export JAVA_HOME="/usr/local/opt/openjdk@21"
+        export PATH="/usr/local/opt/rustup/bin:/opt/local/opt/ccache/libexec:/usr/local/opt/openjdk@21/bin:${HOME}/.cargo/bin:${HOME}/go/bin:${HOME}/.rd/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+        export JAVA_HOME="/usr/local/opt/openjdk@21"
     fi
     export ANDROID_HOME=$HOME/Library/Android/sdk
-    export NDK_VERSION="$(ls -1 $ANDROID_HOME/ndk | tail -n 1)"
-    export NDK_HOME="$ANDROID_HOME/ndk/$NDK_VERSION"
-    export PATH=$PATH:$ANDROID_HOME/emulator
-    export PATH=$PATH:$ANDROID_HOME/platform-tools
     # export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/bottles
     export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles/bottles
     ;;
-  Linux)
+Linux)
     export JAVA_HOME="/usr/lib/jvm/default/"
     export CLASSPATH=${JAVA_HOME}/jre/lib/
     export PATH="/usr/lib/ccache":"${HOME}/go/bin":"${HOME}/.cargo/bin":"${HOME}/.local/bin":${PATH}
     export TERM="xterm-256color"
-    # android about
     export ANDROID_HOME=$HOME/Android/Sdk
+    ;;
+*) ;;
+esac
+
+# android about
+ANDROID_NDK_PARENT_DIR=$ANDROID_HOME/ndk
+if [[ -d "${ANDROID_NDK_PARENT_DIR}" ]]; then
     export NDK_VERSION="$(ls -1 $ANDROID_HOME/ndk | tail -n 1)"
     export NDK_HOME="$ANDROID_HOME/ndk/$NDK_VERSION"
-    export PATH=$PATH:$ANDROID_HOME/emulator
-    export PATH=$PATH:$ANDROID_HOME/platform-tools
-    ;;
-  *) ;;
-esac
+fi
+unset ANDROID_NDK_PARENT_DIR
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
 
 export GOPATH="${HOME}/go/"
 # export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
