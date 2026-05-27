88f17218246456ef35beda61fba9022ab664d9af
whatot <whatot2@gmail.com>
2026-05-27 22:41:49 +0800

    Tune tide prompt selection

--- home/dot_config/fish/conf.d/tide.fish
+++ home/dot_config/fish/conf.d/tide.fish
@@ -2,6 +2,9 @@
 
 set -g tide_left_prompt_items pwd git newline character
 set -g tide_right_prompt_items status cmd_duration jobs direnv time
+set -g tide_git_icon ''
+set -g tide_left_prompt_separator_same_color ' '
+set -g tide_left_prompt_separator_diff_color ' '
 set -g tide_cmd_duration_threshold 1000
 set -g tide_time_format '%H:%M:%S'
 set -g tide_prompt_add_newline_before false
